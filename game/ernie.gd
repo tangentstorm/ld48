b0cfe2ca1520dd9300bf5ca4ca577e78c20cb115
tangentstorm <michal.wallace@gmail.com>
2021-04-24 19:31:50 -0400

    ernie: allow 'coyote time' jumps

--- game/ernie.gd
+++ game/ernie.gd
@@ -1,11 +1,11 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
+var was_on_floor: bool = false
 var dxy : Vector2 = Vector2(0,0)
 const G : Vector2 = Vector2(0,98)
-const SPEED = 100
-const JUMP  = G * -16
+const SPEED = 150
+const JUMP  = G * -8
 const GROUND_FRICTION = Vector2(0.8,1)
 const AIR_FRICTION = Vector2(0.9,1)
 
@@ -22,22 +22,21 @@ func _physics_process(_delta):
 	var goR = (Input.is_key_pressed(KEY_RIGHT) or Input.is_key_pressed(KEY_D)
 		or Input.is_key_pressed(KEY_E))
 
-
-	$"/root/scene/debug".text = "..."
 	if is_on_floor() or is_on_wall():
 		if is_on_wall(): dxy.x = 0
 		if goL: dxy += Vector2.LEFT * SPEED
 		if goR: dxy += Vector2.RIGHT * SPEED
-		if is_on_floor():
-			$"/root/scene/debug".text = "on floor"
-			dxy *= GROUND_FRICTION
-			if Input.is_key_pressed(KEY_SPACE): dxy += JUMP
-	else:
+	if is_on_floor():
+		dxy *= GROUND_FRICTION
+	if is_on_floor() or was_on_floor: # "coyote time" jumps
+		if Input.is_key_pressed(KEY_SPACE): dxy += JUMP
+	if not is_on_floor():
 		dxy *= AIR_FRICTION
 		# allow small amount of steering in the air
 		if goL: dxy += Vector2.LEFT * SPEED/3
 		if goR: dxy += Vector2.RIGHT * SPEED/3
 
-
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().reload_current_scene()
+
+	was_on_floor = is_on_floor()
